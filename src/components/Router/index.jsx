149a9338bee544bdb13e0387eef72c21c8c5fa00
charlie632 <charlie.caoa@gmail.com>
2020-07-17 14:20:04 -0500

    feat: add protected route component

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -2,16 +2,27 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Layout from '../Layout';
 import Home from '../Home';
+import { useGeneralContext } from '../../context/General';
 
 const Favorites = () => (
   <div>Favorites</div>
 );
 
+const ProtectedRoute = ({ ...props }) => {
+  const [{ user }] = useGeneralContext();
+
+  if (!user) {
+    return <div>nope</div>;
+  }
+
+  return <Route {...props} />;
+};
+
 const Router = () => (
   <BrowserRouter>
     <Layout>
       <Switch>
-        <Route component={Favorites} path="/favorites" exact />
+        <ProtectedRoute component={Favorites} path="/favorites" exact />
         <Route component={Home} path="/" />
       </Switch>
     </Layout>
